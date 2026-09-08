6fcc28adb899547d6f1db442642e0ffda1866a92
gjdbsan1234-netizen <gjdbsan1234@gmail.com>
2026-09-08 21:58:09 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-const CACHE="tongin-db-v6";const ASSETS=["./","./index.html","./manifest.json","./sw.js"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="tongin-db-v7";
+const ASSETS=["./","./index.html","./manifest.json","./sw.js"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{
+  const r=e.request;
+  if(r.mode==="navigate" || new URL(r.url).pathname.endsWith("/index.html")){
+    e.respondWith(fetch(r).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(r,copy));return resp}).catch(()=>caches.match(r).then(x=>x||caches.match("./index.html"))));
+    return;
+  }
+  e.respondWith(caches.match(r).then(cached=>cached||fetch(r).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(r,copy));return resp})));
+});
